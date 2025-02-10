8f6ce6a1bd0970ef24f6ad1f4b15cad49d5d6610
Lord <volin232c@gmail.com>
2025-02-10 22:10:04 +0200

    task4 body

---
+++ Task4/Models/Category.cs
@@ -0,0 +1,17 @@
+/// <summary>
+/// Перелічуваний тип даних для категорій товарів.
+/// </summary>
+public enum Category
+{
+    /// <summary>Продукти харчування</summary>
+    Food,
+    
+    /// <summary>Електроніка</summary>
+    Electronics,
+    
+    /// <summary>Одяг</summary>
+    Clothing,
+    
+    /// <summary>Домашнє обладнання</summary>
+    HomeAppliance
+}
